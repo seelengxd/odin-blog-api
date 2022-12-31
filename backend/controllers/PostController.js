b86d25d1542b44482b94f7548e156a55612d23e7
see leng <seelengxd@gmail.com>
2022-12-31 17:03:49 +0800

    feat: add dummy post routes

---
+++ backend/controllers/PostController.js
@@ -0,0 +1,19 @@
+exports.index = (req, res, next) => {
+  res.json({ message: "Index: not implemented" });
+};
+
+exports.create = (req, res, next) => {
+  res.json({ message: "Create: not implemented" });
+};
+
+exports.update = (req, res, next) => {
+  res.json({ message: "Update: not implemented" });
+};
+
+exports.delete = (req, res, next) => {
+  res.json({ message: "Delete: not implemented" });
+};
+
+exports.createComment = (req, res, next) => {
+  res.json({ message: "Create comment: not implemented" });
+};
---
+++ backend/routes/posts.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const PostController = require("../controllers/PostController");
+
+router.get("/", PostController.index);
+
+router.post("/", PostController.create);
+
+router.put("/:id", PostController.update);
+
+router.delete("/:id", PostController.delete);
+
+router.post("/:id/comments", PostController.createComment);
+
+module.exports = router;
