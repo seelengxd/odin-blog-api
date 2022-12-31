c6780d5de9876cf80e451f7050e173b465a5ef29
see leng <seelengxd@gmail.com>
2022-12-31 17:33:30 +0800

    feat: add CRUD methods for posts

--- backend/controllers/PostController.js
+++ backend/controllers/PostController.js
@@ -1,17 +1,66 @@
+const Post = require("../models/post");
+const { body, validationResult } = require("express-validator");
+
+const postValidators = [
+  body("title", "Title cannot be empty!").isLength({ min: 1 }),
+  body("content", "Content cannot be empty").isLength({ min: 1 }),
+];
+
 exports.index = (req, res, next) => {
-  res.json({ message: "Index: not implemented" });
+  Post.find({ ...req.query }, (err, posts) => {
+    if (err) {
+      next(err);
+    }
+    res.json({ posts });
+  });
 };
 
-exports.create = (req, res, next) => {
-  res.json({ message: "Create: not implemented" });
-};
+exports.create = [
+  postValidators,
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json(errors);
+    } else {
+      new Post(req.body).save((err) => {
+        if (err) {
+          next(err);
+        } else {
+          res.sendStatus(200);
+        }
+      });
+    }
+  },
+];
 
-exports.update = (req, res, next) => {
-  res.json({ message: "Update: not implemented" });
-};
+exports.update = [
+  postValidators,
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json(errors);
+    } else {
+      Post.findByIdAndUpdate(req.params.id, req.body, (err) => {
+        if (err) {
+          res
+            .status(400)
+            .json({ errors: [{ msg: "ID probably doesn't exist" }] });
+        } else {
+          res.sendStatus(200);
+        }
+      });
+    }
+  },
+];
 
 exports.delete = (req, res, next) => {
-  res.json({ message: "Delete: not implemented" });
+  Post.findByIdAndDelete(req.params.id, (err) => {
+    if (err) {
+      res.status(400).json({ errors: [{ msg: "ID probably doesn't exist" }] });
+    } else {
+      res.sendStatus(200);
+    }
+  });
 };
 
 exports.createComment = (req, res, next) => {
